393b90382ad43774ecfabd39d7c9cc49c7176721
Stefanski Krzysztof <krzysztof.stefanski@nokia.com>
2023-12-04 18:32:44 +0100

    fix 2

--- php/templates/database/admin/admin_send.php
+++ php/templates/database/admin/admin_send.php
@@ -10,17 +10,14 @@ $request = $user->getRequest();
 if (isset($_POST['accept']) || isset($_POST['save']) || isset($_POST['deny']) || isset($_POST['block'])) {
     $admin_comment = filter_input(INPUT_POST, 'admin_comment', FILTER_SANITIZE_STRING);
 
-
-
     if (isset($_POST['accept'])) {
         $update_query = $db->prepare("UPDATE requests SET status=8, admin_comment=:admin_comment WHERE eid =:user_eid");
 
         if ($active['status'] != 0) {
             $insert_active_request = $db->prepare("UPDATE active_requests SET bid=:bid, bsid=:bsid, building3=:building3, building2=:building2, building1=:building1, room4=:room4, room3=:room3, room2=:room2, room1=:room1, room3_justification=:room3_justification, room2_justification=:room2_justification, room1_justification=:room1_justification, room4_justification=:room4_justification, activaiton_day=:activaiton_day, status=1 WHERE eid=:user_eid");
         } else {
-            $insert_active_request = $db->prepare("INSERT INTO active_requests(eid, bid, bsid, building3, building2, building1, room4, room3, room2, room1, room3_justification, room2_justification, room1_justification, room4_justification, activaiton_day, status) VALUES (:user_eid, :bid, :bsid, :building3, :building2, :building1, :room4, :room3, :room2, :room1, :room3_justification, :room2_justification, :room1_justification, :room4_justification, :activaiton_day, 1)");
+            $insert_active_request = $db->prepare("INSERT INTO active_requests(eid, bid, bsid, building3, building2, building1, room4, room3, room2, room1, room3_justification, room2_justification, room1_justification, room4_justification, activaiton_day, `status`) VALUES (:user_eid, :bid, :bsid, :building3, :building2, :building1, :room4, :room3, :room2, :room1, :room3_justification, :room2_justification, :room1_justification, :room4_justification, :activaiton_day, 1)");
         }
-
 
         $insert_active_request->bindValue(':user_eid', $usr['user'], PDO::PARAM_STR);
         $insert_active_request->bindValue(':bid', $request['bid'], PDO::PARAM_STR);
@@ -34,14 +31,16 @@ if (isset($_POST['accept']) || isset($_POST['save']) || isset($_POST['deny']) ||
         $insert_active_request->bindValue(':room2', $request['room2'], PDO::PARAM_BOOL);
         $insert_active_request->bindValue(':room1', $request['room1'], PDO::PARAM_BOOL);
 
-        $insert_active_request->bindValue(':activaiton_day', date('Y-m-d'), PDO::PARAM_BOOL);
+        $insert_active_request->bindValue(':activaiton_day', date('Y-m-d'), PDO::PARAM_STR);
 
         $insert_active_request->bindValue(':room3_justification', $request['room3_justification'], PDO::PARAM_STR);
         $insert_active_request->bindValue(':room2_justification', $request['room2_justification'], PDO::PARAM_STR);
         $insert_active_request->bindValue(':room1_justification', $request['room1_justification'], PDO::PARAM_STR);
         $insert_active_request->bindValue(':room4_justification', $request['room4_justification'], PDO::PARAM_STR);
+
+
         
-        $insert_active_request->execute();
+        // $insert_active_request->execute();
         
     } elseif (isset($_POST['save'])) {
         $_SESSION['review_alert'] = 'Save complited';
